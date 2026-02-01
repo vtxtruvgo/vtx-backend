e44fa13c3cdb9f42610a21c52edb5517fe1cf040
Varshith-code <varshith.code@gmail.com>
2026-02-01 16:07:53 +0530

    Fix Syntax Error in App.js

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use('/status', statusRoutes); // Accessible at /status (HTML) and /status?fo
 
 if (process.env.NODE_ENV !== 'production') {
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(\Server running on port \\));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
 export default app;
